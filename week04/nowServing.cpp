571cd8908ee6367d1881b86bdb569a33602125db
Cody Nichols <canichols2@gmail.com>
2018-02-03 14:47:03 -0700

    implemented now serving. works locally. sending to linuxlab for testbed.

--- week04/nowServing.cpp
+++ week04/nowServing.cpp
@@ -30,6 +30,58 @@ void nowServing()
    cout << "\tfinished                     : end simulation\n";
 
    // your code here
+   string command;
+   string csClass;
+   string name;
+   int min;
+   int curMin=0;
+   struct student { student(){name="";csClass="";min=1;} student(string n,string c, int m){name = n;csClass = c; min = m;} string name; string csClass; int min; };
+   student currentStudent;
+   Deque<student> queue;
+   do
+   {
+      cout << "<"<< curMin++ << "> ";
+      cin  >> command;
+
+      try
+      {
+         if (command == "!!")
+         {
+            cin >> csClass >> name >> min;
+            queue.push_front(student(name,csClass,min) );
+         }
+         else if (command == "none")
+         {
+            // Do nothing
+         }
+         else if (command == "finished")
+         {
+            break;
+         }
+         else
+         {
+            csClass = command;
+            cin >> name >> min;
+            queue.push_front(student(name,csClass,min) );
+         }
+         //TODO: Decrement current student time
+         currentStudent.min--;
+         //TODO: if current studnet time up, remove and grab new student.
+         if(currentStudent.min <= 0 && queue.size()){
+            currentStudent=queue.front();
+            queue.pop_front();
+         }
+         if(currentStudent.min > 0){
+            //TODO: output the current student
+            cout << "\tCurrently serving " << currentStudent.name << " for class "<<currentStudent.csClass<<". Time left: "<< currentStudent.min<<endl;
+         }
+      }
+      catch (const char * e)
+      {
+         cout << '\t' << e << endl;
+      }
+   }
+   while (command != "!");
 
    // end
    cout << "End of simulation\n";

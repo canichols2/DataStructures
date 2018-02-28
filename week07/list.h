db9ca4f3dd7529c242e5399b8e3551e4aad93254
Cody Nichols <canichols2@gmail.com>
2018-02-27 23:17:25 -0700

    Implemented WholeNumber with Linked List of ints. can go however high of a number i want now.

--- week07/list.h
+++ week07/list.h
@@ -211,29 +211,6 @@ void List<T>::freeData(Node<T>* &ptr){
    ptr = NULL;
 }
 
-//Constructors: Default constructor (create an empty list) and the copy constructor. If allocation is not possible, the following error will be thrown:
-//ERROR: unable to allocate a new node for a list
-//Destructor: When finished, the class should delete all the allocated memory.
-//operator=(): Removes all the items in the current list and copies the contents from the right-hand side (rhs) onto the current list. In the case of an allocation error, the following c-string will be thrown:
-//ERROR: unable to allocate a new node for a list 
-//empty(): Test whether the list is empty. This method takes no parameters and returns a Boolean value.
-//clear(): Empties the list of all items. There are no parameters and no return value.
-//size(): Returns the number of nodes in the list. There are no parameters and the return value is an integer.
-//push_back(): Adds an item to the back of the list. This method takes a single parameter (the item to be added to the end of the list) and has no return value. In the case of an allocation error, the following c-string exception will be thrown:
-//ERROR: unable to allocate a new node for a list
-//push_front(): Adds an item to the front of the list exactly the same as push_back().
-//front(): Returns the item currently at the front of the list. This item is returned by-reference, so the last item can be changed through the front() method. If the list is currently empty, the following exception will be thrown:
-//ERROR: unable to access data from an empty list
-//back(): Returns the item currently at the back of the list exactly the same as front().
-//insert(): Inserts an item in the middle of a list. There are two parameters: the data item to be inserted, and a ListIterator pointing to the location in the list where the new item will be inserted before. The return value is an iterator to the newly inserted item. In the case of an allocation error, the following exception will be thrown:
-//ERROR: unable to allocate a new node for a list
-//remove(): Removes an item from the middle of a list. There is one parameter: a ListIterator pointing to the item to be removed. In the case of an iterator pointing to end(), the following exception will be thrown:
-//ERROR: unable to remove from an invalid location in a list
-//begin(): Return an iterator to the first element in the list. It takes no parameters and returns a ListIterator.
-//rbegin(): Return an iterator to the last element in the list. It takes no parameters and returns a ListIterator. Note that this will behave differently than the STL rbegin() method which returns a std::list <T> :: reverse_iterator.
-//end(): Return an iterator referring to the past-the-end element in the list. The past-the-end element is the theoretical element that would follow the last element in the container. It does not point to any element, so it must not be de-referenced.
-//rend(): Return an iterator referring to the past-the-front element in the list. The past-the-front element is the theoretical element that would preceed the first element in the container. It does not point to any element, so it must not be de-referenced. Note that this will behave differently than the STL rend() method which returns a std::list <T> :: reverse_iterator.
-
 /**************************************************
  * VECTOR ITERATOR
  * An iterator through Vector
@@ -262,27 +239,32 @@ class ListIterator
    // prefix increment
    ListIterator <T> & operator ++ ()
    {
-      p = p->pNext;
+      if(p != NULL)
+         p = p->pNext;
       return *this;
    }
    // postfix increment
    ListIterator <T> operator++(int postfix)
    {
       ListIterator tmp(*this);
-      p = p->pNext;
+      if(p != NULL)
+         p = p->pNext;
       return tmp;
    }
    // prefix decrement
    ListIterator <T> & operator -- ()
    {
-      p = p->pPrev;
+
+      if(p != NULL)
+         p = p->pPrev;
       return *this;
    }
    // postfix decrement
    ListIterator <T> operator--(int postfix)
    {
       ListIterator tmp(*this);
-      p = p->pPrev;
+      if(p != NULL)
+         p = p->pPrev;
       return tmp;
    }
    friend ListIterator<T>  List<T>::insert(ListIterator<T> it,T item);
--- week07/whole.h
+++ week07/whole.h
@@ -9,22 +9,49 @@ using namespace std;
 class WholeNumber
 {
    private:
-      int number;
-   public:
-      WholeNumber():number(0){}
-      WholeNumber(int num):number(num){}
-      WholeNumber(const WholeNumber& num){
-         this->number = num.number;
+      List<int> number;
+      int val(ListIterator<int> it){
+         if(it != NULL)
+            return *it;
+         return 0;
       }
-      WholeNumber& operator+=(const WholeNumber& rhs){
-         number += rhs.number;
+      // int number;
+   public:
+      WholeNumber(){
+         number.push_back(0);
+      }
+      WholeNumber(int num){
+         number.push_back(num);
+      }
+      WholeNumber(const WholeNumber& num){
+         //calls list assignment operator
+         number = num.number;
+      }
+      WholeNumber& operator+=(WholeNumber& rhs){
+         ListIterator<int> itLhs = number.rbegin();
+         ListIterator<int> itRhs = rhs.number.rbegin();
+         List<int> newList = List<int>();
+         int over = 0;
+         while(itLhs != NULL || itRhs != NULL){
+            int total = val(itLhs) + val(itRhs) + over;
+            over = total / 1000;
+            int newTotal = total % 1000;
+            newList.push_front(newTotal);
+            itLhs--;itRhs--;
+         }
+         if(over > 0){
+            // make new node
+            newList.push_front(over);
+         }
+         number = newList;
+         // number += rhs.number;
          return *this;
       }
-      WholeNumber& operator=(const WholeNumber& rhs){
+      WholeNumber& operator=( WholeNumber& rhs){
          number = rhs.number;
          return *this;
       }
-      WholeNumber operator+(const WholeNumber& rhs){
+      WholeNumber operator+( WholeNumber& rhs){
          WholeNumber tmp = *this;
          tmp += rhs;
          return tmp;
@@ -34,8 +61,25 @@ class WholeNumber
 
 
 ostream& operator << (ostream& out,const  WholeNumber &rhs)
-{
-   out <<rhs.number;
+{ 
+   WholeNumber tmp = WholeNumber(rhs); 
+   ListIterator<int> it = tmp.number.begin();
+   //Display the first set without leading 0's
+   out << *it;
+   it++;
+   if(it != NULL)
+      out<<",";
+   while (it != NULL){
+      if(*it < 100) 
+         out << 0;
+      if(*it < 10) 
+         out << 0;
+      out << *it;
+      it++;
+      if(it != NULL)
+         out<<",";
+   }
+      
    return out;
 }
 #endif

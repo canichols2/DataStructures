a10744f1066755ae9c2982789736e32705ac1388
cody allen nichols <nicho92@LinuxLab01.byui.edu>
2018-03-02 18:35:48 -0700

    adding starting week* things

---
+++ week08/bnode.h
@@ -0,0 +1,39 @@
+#ifndef BNODE_H
+#define BNODE_H
+
+#include <iostream> 
+#include <cassert>
+
+/*****************************************************************
+ * BINARY NODE
+ * A single node in a binary tree.  Note that the node does not know
+ * anything about the properties of the tree so no validation can be done.
+ *****************************************************************/
+template <class T>
+class BinaryNode
+{
+public:
+   // return size (i.e. number of nodes in tree)
+   int size() const
+   {
+            return 1 +
+               (pLeft== NULL? 0 : pLeft->size()) +
+               (pRight == NULL ? 0 : pRight->size());
+   }
+   
+   // add a node to the left/right
+   void addLeft (BinaryNode <T> * pNode);
+   void addRight(BinaryNode <T> * pNode);
+
+   // create a node and add it to the left/right
+   void addLeft (const T & t) throw (const char *);
+   void addRight(const T & t) throw (const char *);
+   
+   // since no validation is done, everything is public
+   BinaryNode <T> * pLeft;
+   BinaryNode <T> * pRight;
+   BinaryNode <T> * pParent;
+   T data;
+};
+
+#endif // BNODE_H

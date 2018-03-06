98d4a08c3eadbd832139ca4a5c7e663e165382f5
Cody Nichols <canichols2@gmail.com>
2018-03-05 18:55:37 -0700

    Changed read to display the values in order, not in order of tree. Changed huffman insert validations to insert in order where if the values match, the one that was already there will stay in front.

--- week08/huffman.cpp
+++ week08/huffman.cpp
@@ -26,46 +26,20 @@ void readTree(BinaryNode<Pair<float,string> >* node,string huffStr);
 void huffman(string fileName)
 {
       huff huffObj;
-   // List< // A Sorted List of
-   //    BinaryNode< //Binary Nodes of
-   //       Pair<    //of Pair's of
-   //          float,//Floats and
-   //          string//Strings
-   //          > 
-   //       >*
-   //    > sortedNodes = List<BinaryNode<Pair<float,string> >*>();
-   // List<Pair<float,string> > sortedThings = List<Pair<float,string> >();
    float tmpF;
    string tmpS;
    ifstream file(fileName.c_str());
    while(file>>tmpS>>tmpF)
    {
       //I created a new insert to sort as it's inserting.
-      huffObj.insertOrdered( new BinaryNode<Pair<float,string> >(  Pair<float,string>(tmpF,tmpS))  );
-      // ListIterator<BinaryNode<Pair<float,string> >*> it = sortedNodes.begin();
-      // cout << huffObj <<endl;
-      // while(it != NULL){
-      //    cout << *it << endl;
-      //    it++;
-      // }
+      huffObj.rInsertOrdered( new BinaryNode<Pair<float,string> >(  Pair<float,string>(tmpF,tmpS))  );
+      cout << huffObj <<endl;
+      
    }
-   //for each Node in List
-      //Until there is only one left to sort into the tree
-   //pop Node off onto tmp* variable.
-   //Pop next Node off onto tmp2* variable
-   //make new node 
-      //with addition of 2 floats and an empty string
-      //With both tmp* and tmp2* nodes as children
-      //Smaller on left
-   //rInsertOrdered back onto list
    
    //TODO Call huffObj. Create Tree...
    huffObj.createTree();
 
-   //set key values
-      //Read through each node, starting with the left side
-      //Write all values into some variable
-      //using "key = 1101" format
    huffObj.readTree();
 //    readTree(sortedNodes.front(),"");
 
@@ -73,18 +47,5 @@ void huffman(string fileName)
 }
 
 
-/**********************
- * Recursively read through a Node tree
- * cout when you reach the end of the tree
- * str = 11101
- * ******************/
-// void readTree(BinaryNode<Pair<float,string> >* node,string huffStr){
-//    if(node->pLeft != NULL && node->pRight != NULL){
-//       readTree(node->pLeft,huffStr +"0");
-//       readTree(node->pRight,huffStr +"1");
-//    }else{
-//       cout << node->data.second << " = " << huffStr<<endl;
-//    }
-// }
 
 
--- week08/huffman.h
+++ week08/huffman.h
@@ -22,19 +22,30 @@ class huff
 {
       private:
          List<BinaryNode<Pair<float,string> >*> orderedList;
+         List<Pair<string,string> > endList;
          void readTree(BinaryNode<Pair<float,string> >* node,string huffStr)
          {
             if(node->pLeft != NULL && node->pRight != NULL){
                readTree(node->pLeft,huffStr +"0");
                readTree(node->pRight,huffStr +"1");
             }else{
-               cout << node->data.second << " = " << huffStr<<endl;
+               // cout << node->data.second << " = " << huffStr<<endl;
+               endList.insertOrdered( Pair<string,string>(node->data.second,huffStr));
             }
          }
       public:
       huff():orderedList(){}
-
+      friend ostream & operator << (ostream & out, huff & rhs) {
+		  out << "Huff Ordered List: ";
+		  out << '{';
+		  ListIterator <BinaryNode<Pair<float, string > >*> it;
+		  for (it = rhs.orderedList.begin(); it != rhs.orderedList.end(); ++it)
+			  out << "  " << (*it)->data;
+		  out << "  }";
+		  return out;
+	  }
       void createTree(){
+         cerr << "Creating Tree"<<endl; 
          while (orderedList.size() > 1)
          {
                string tmpStr = "";
@@ -46,15 +57,25 @@ class huff
                      );
                node->addLeft(tmp1);
                node->addRight(tmp2);
-               orderedList.insertOrdered(node);
-               // cout << node;
+               rInsertOrdered(node);
+               cout << *this<<endl;
          }
       }
       void readTree()
       {
          readTree(orderedList.front(),"");
+         for(ListIterator<Pair<string,string> > it = endList.begin(); it != NULL; it++)
+         {
+            cout << (*it).first << " = " << (*it).second<<endl;
+         }
       }
 
+      /***************************
+       * Insert Ordered. 
+       * Needed because the function in
+       * the List object doesnt' work
+       * if the list contains object pointers
+       * ****************************/
       ListIterator<BinaryNode<Pair<float,string> >*> insertOrdered(BinaryNode<Pair<float,string> >* item){
             return insertOrdered(orderedList.begin(),item);
       }
@@ -63,7 +84,7 @@ class huff
             if(
                !(
                it != NULL) || 
-               item <= *it)
+               item->data <= (*it)->data)
             {
                   return orderedList.insert(it,item);
             }else{
@@ -76,12 +97,15 @@ class huff
       }
       ListIterator<BinaryNode<Pair<float,string> >*> rInsertOrdered(ListIterator<BinaryNode<Pair<float,string> >*> it,BinaryNode<Pair<float,string> >* item)
       {
-            if(!(it != NULL)  || item >= *it){
-                  it++;
-                  return orderedList.insert(it,item);
+            if(!(it != NULL)){
+               it = orderedList.begin();
+               return orderedList.insert(it,item);
+            } else if(item->data >= (*it)->data){
+               it++;
+               return orderedList.insert(it,item);
             }else{
-                  it--;
-                  return rInsertOrdered(it,item);
+               it--;
+               return rInsertOrdered(it,item);
             }
       }
 
@@ -96,6 +120,7 @@ class huff
 
 
 //Temporary non-member functions to output BinaryNode and Pair
+
 template <class T>
 ostream & operator << (ostream & out, BinaryNode<T>* & rhs){
    // "  { D:{lBN}{rBN} }  ";
@@ -110,47 +135,27 @@ ostream & operator << (ostream & out, BinaryNode<T>* & rhs){
  * LIST :: DISPLAY
  * Display the contents of the list forwards
  ****************************************/
-template <class T>
-ostream & operator << (ostream & out, List <T> & rhs)
-{
-   out << '{';
 
-// #ifdef TEST3
-   ListIterator <T> it;
-   for (it = rhs.begin(); it != rhs.end(); ++it)
-      out << "  " << (*it)->data;
-// #endif // TEST3
-
-   out << "  }";
-
-   return out;   
-}
-
-
-
-
-
-
-// class huff
+// ostream & operator << (ostream & out, List<BinaryNode<Pair<float, string> >*> & rhs)
 // {
-//    private:
-//       BinaryNode<pair<float,string> >* bHead;
-//       List<BinaryNode<pair<float,string> >*> orderedList;
-//       // pair<string,int> keys[];
-//    public:
-//       huff():bHead(NULL){}
-//       huff(BinaryNode<pair<float,string> >* bNode):bHead(bNode){}
-      
-//       void addNode(BinaryNode<pair<float,string> > bNode){}
-//       void getKey(){}
-// };
+//    out << '{';
+//    ListIterator <BinaryNode<Pair<float,string > >*> it;
+//    for (it = rhs.begin(); it != rhs.end(); ++it)
+//       out << "  " << (*it)->data;
+//    out << "  }";
 
+//    return out;   
+// }
 
-
-
-
-
-
-
+//
+//ostream & operator << (ostream & out, huff & rhs){
+//   out << "Huff Ordered List: ";
+//   out << '{';
+//   ListIterator <BinaryNode<Pair<float,string > >*> it;
+//   for (it = rhs.orderedList.begin(); it != rhs.orderedList.end(); ++it)
+//      out << "  " << (*it)->data;
+//   out << "  }";
+//   return out;
+//}
 
 #endif // HUFFMAN_h
